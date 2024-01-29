eaa70121eed3c488aa0f5300f78dc9a472263f32
Divya Chahal <dchahal@apac.corpdir.net>
2024-01-29 15:49:51 +0530

    initial commit

---
+++ ContactWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
