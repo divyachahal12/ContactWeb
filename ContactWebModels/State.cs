dee3489321ee06ba7023d1fc61daf8a59c9186d8
Divya Chahal <dchahal@apac.corpdir.net>
2024-01-30 16:26:32 +0530

    created models project

---
+++ ContactWebModels/State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactWebModels
+{
+    public class State
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "State")]
+        [Required(ErrorMessage = "Name of State is Required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_NAME_LENGTH)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "State Abbreviation is Required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_ABBR_LENGTH)]
+        public string Abbreviation { get; set; }
+    }
+}
